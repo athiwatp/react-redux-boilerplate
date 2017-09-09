3d410e2e365dc9babf0b549d0e593d6f8d2a455d
niqdev <niqdev@gmail.com>
2017-09-09 18:25:55 +0100

    split webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,10 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  devServer: {
-    contentBase: './dist'
+  entry: {
+    app: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
@@ -16,7 +15,7 @@ module.exports = {
     })
   ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
